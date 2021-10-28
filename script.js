fd39aa554d69d5e720da1405d3ce323520e9b7ba
phillipsalangsang <phillip_salangsang@hotmail.com>
2021-10-28 15:43:50 +0100

    Made changes to the JS

--- script.js
+++ script.js
@@ -20,81 +20,155 @@
 
 // alert (story)
 
+// "use strict"
+
+// const numHorses = 5
+// let horses=[]
+// let horseX=[]
+
+// let colors=["red", "blue", "orange", "cyan", "magenta", "yellow", "black", "turquoise", "grey", "lilac", "white"]
+
+// function startRace(){
+//     setInterval(move,50)
+// }
+
+// //setup all the horses
+// for(let h=0;h<numHorses;h++){
+//     horses[h] = document.createElement("div")
+//     horses[h].classList.add("horse")
+//     horses[h].style.top = 50 + h*18 + "px"
+//     horses[h].style.backgroundColor=colors[h]
+//     horseX[h]=0
+//     document.body.appendChild(horses[h])
+// }
+
+// const finish=500
+
+// function move(){
+//     for(let h=0;h<numHorses;h++){
+//         horses[h].style.left=horseX[h] +"px"
+//     }
+// }
+
+// var redHorse=document.getElementById("redHorse")
+// var blueHorse=document.getElementById("blueHorse")
+// var yellowHorse=document.getElementById("yellowHorse")
+// var blackHorse=document.getElementById("blackHorse")
+
+// var redHorseX=0
+// var blueHorseX=0
+// var yellowHorseX=0
+// var blackHorseX=0
+
+// const finish=800
+
+// function move(){
+    
+//     redHorse.style.left = redHorseX +"px"
+//     redHorseX += Math.floor(Math.random()*10)
+    
+//     blueHorse.style.left = blueHorseX +"px"
+//     blueHorseX += Math.floor(Math.random()*10)
+
+//     yellowHorse.style.left = yellowHorseX +"px"
+//     yellowHorseX += Math.floor(Math.random()*10)
+    
+//     blackHorse.style.left = blackHorseX +"px"
+//     blackHorseX += Math.floor(Math.random()*10)
+    
+//     if (redHorseX>=800){alert("Red horse wins");resetRace()}
+//     if (blueHorseX>=800){alert("Blue horse wins");resetRace()}
+//     if (yellowHorseX>=800){alert("Yellow horse wins");resetRace()}
+//     if (blackHorseX>=800){alert("Black horse wins");resetRace()}
+// }
+
+// function startRace(){
+
+//     setInterval(move,50)  
+//     alert("On Your Marks. Get Set. GO!")
+//     clearInterval()
+// }
+
+// function resetRace(){
+//     redHorseX=0
+//     blueHorseX=0
+//     yellowHorseX=0
+//     blackHorseX=0
+// }
+
+
 "use strict"
+
+class Horse{
+
+    element 
+    x 
+
+    constructor(element,y,color){
+
+        element.style.top = y + "px"
+        element.style.backgroundColor=color
+        element.classList.add("horse")
+        document.body.appendChild(element)
+
+        this.element=element
+        this.x=0
+
+}
+
+    move(){
+        this.element.style.left=this.x +"px"
+        this.x += Math.floor(Math.random()*10)
+}
+
+    checkForWin(){
+        if(this.x >=finish){
+            alert(`$(colors[h]} horse wins`)
+            resetRace();
+    }
+}
+
+    rest(){
+        this.x=0
+    }    
+}
 
 const numHorses = 5
 let horses=[]
-let horseX=[]
 
-let colors=["red", "blue", "orange", "cyan", "magenta", "yellow", "black", "turquoise", "grey", "lilac", "white"]
+
+let colors=["red","blue","orange","cyan","magenta","yellow","black","turqoise","grey","lilac","white"]
 
 function startRace(){
-    setInterval(move,50)
+    setInterval(moveHorses,50)
 }
 
-//setup all the horses
-for(let h=0;h<numHorses;h++){
-    horses[h] = document.createElement("div")
-    horses[h].classList.add("horse")
-    horses[h].style.top = 50 + h*18 + "px"
-    horses[h].style.backgroundColor=colors[h]
-    horseX[h]=0
-    document.body.appendChild(horses[h])
+function setupHorses(){
+    for(let h=0;h<numHorses;h++){
+        horses.push (new Horse(document.createElement("div"), 50 + h*45,colors[h]))
+    }
 }
 
 const finish=500
 
-function move(){
+function moveHorses(){
     for(let h=0;h<numHorses;h++){
-        horses[h].style.left=horseX[h] +"px"
+
+        let horse=horses[h]
+        horse.move()
+        horse.checkForWin()
     }
 }
 
-var redHorse=document.getElementById("redHorse")
-var blueHorse=document.getElementById("blueHorse")
-var yellowHorse=document.getElementById("yellowHorse")
-var blackHorse=document.getElementById("blackHorse")
-
-var redHorseX=0
-var blueHorseX=0
-var yellowHorseX=0
-var blackHorseX=0
-
-const finish=800
-
-function move(){
-    
-    redHorse.style.left = redHorseX +"px"
-    redHorseX += Math.floor(Math.random()*10)
-    
-    blueHorse.style.left = blueHorseX +"px"
-    blueHorseX += Math.floor(Math.random()*10)
-
-    yellowHorse.style.left = yellowHorseX +"px"
-    yellowHorseX += Math.floor(Math.random()*10)
-    
-    blackHorse.style.left = blackHorseX +"px"
-    blackHorseX += Math.floor(Math.random()*10)
-    
-    if (redHorseX>=800){alert("Red horse wins");resetRace()}
-    if (blueHorseX>=800){alert("Blue horse wins");resetRace()}
-    if (yellowHorseX>=800){alert("Yellow horse wins");resetRace()}
-    if (blackHorseX>=800){alert("Black horse wins");resetRace()}
-}
-
-function startRace(){
-
-    setInterval(move,50)  
-    alert("On Your Marks. Get Set. GO!")
-    clearInterval()
-}
 
 function resetRace(){
-    redHorseX=0
-    blueHorseX=0
-    yellowHorseX=0
-    blackHorseX=0
+    for(let h=0;h<numHorses;h++){
+        horse[h.reset()]
+    }
 }
+
+setupHorses()
+
 
 
 
